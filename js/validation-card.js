55eaafc674dd4ecca2e1df6ec48418a9f64805ea
HunterAkeriano <gamerstaject@gmail.com>
2023-02-24 20:32:32 +0200

    add validation

--- js/validation-card.js
+++ js/validation-card.js
@@ -125,3 +125,47 @@ cvvust.addEventListener('input',   () => {
     }
   });
 
+
+
+
+//   Валидация карты телефона
+
+const cardPhoneUser = document.querySelector('.card-phone');
+cardPhoneUser.addEventListener("input",(ev)=>{
+    // не позволяем ввести ничего, кроме цифр 0-9, ограничиваем размер поля 19-ю символами
+    if( ev.inputType === "insertText" && !numbers.test(ev.data) || cardPhoneUser.value.length > 19){
+        cardPhoneUser.value = cardPhoneUser.value.slice(0, cardPhoneUser.value.length - 1)
+        return
+    }
+
+    // обеспечиваем работу клавиш "backspace","delete"
+    let value = cardPhoneUser.value
+    if( ev.inputType === "deleteContentBackward" && regExp.test(value.slice(-4)) ){
+        cardPhoneUser.value = cardPhoneUser.value.slice(0, cardPhoneUser.value.length - 1)
+        return
+    }
+
+    // добавяем пробел после 4 цифр подряд
+    if( regExp.test(value.slice(-4)) && value.length < 19){
+        cardPhoneUser.value += " "
+    }
+})
+
+
+
+let datePhone = document.querySelector('.date-phone');
+datePhone.addEventListener('input',   () => {
+    if (datePhone.value.length > 5) {
+        datePhone.value = datePhone.value.slice(0, 5);
+    }
+  });
+
+
+const phoneCVV = document.querySelector('.phone-cvv');
+phoneCVV.addEventListener('input',   () => {
+    if (phoneCVV.value.length > 3) {
+        phoneCVV.value = phoneCVV.value.slice(0, 3);
+    }
+  });
+
+
