8518a1eec2012055791aa126b8303ae5da687338
HunterAkeriano <gamerstaject@gmail.com>
2023-02-27 15:52:22 +0200

    Добавление страниц

---
+++ .history/send_20230227151435.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $to = "stajectgamer@gmail.com"; // замените на свой адрес электронной почты
+  $subject = "Сообщение поддержки";
+  $name = strip_tags($_POST["name"]);
+  $email = strip_tags($_POST["email"]);
+  $message = strip_tags($_POST["message"]);
+  $headers = "From: $name <$email>" . "\r\n" .
+             "Reply-To: $email" . "\r\n" .
+             "X-Mailer: PHP/" . phpversion();
+
+  mail($to, $subject, $message, $headers);
+
+  header("Location: support.html"); // замените на страницу благодарности на своем сайте
+  exit;
+}
+?>
